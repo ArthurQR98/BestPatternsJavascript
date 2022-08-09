2f13381479c988794911737095c31d334ff274f3
ArthurQRPE98 <arthur.quezada98@gmail.com>
2022-08-09 11:46:06 -0500

    Add patterns creationals [Pattern Builder, Pattern Injection Depency, Pattern Singleton]

---
+++ Creacionales/Patron Builder/index.js
@@ -0,0 +1,10 @@
+// node index.js
+import { UrlBuilder } from "./urlBuilder.js";
+
+const url = new UrlBuilder()
+  .setProtocol("https")
+  .setAuthentication("user", "pass")
+  .setHostname("example.com")
+  .build();
+
+console.log(url.toString());
---
+++ inmutabilidad.js
@@ -0,0 +1,17 @@
+const obj = {
+    name: "Charly",
+    age: 23
+}
+
+const obj2 = {
+    ...obj,
+    name: "Arthur",
+    country: "Brasil",
+    opocution: "Ing.Sistems"
+}
+
+// const obj3 = {
+//     ...obj,
+// }
+
+console.log(obj, obj2);
